61eb6163055b1ab47ef9c7cde64b3b37fc9791a6
Tizzio <adriano.tummi@hotmail.it>
2015-07-28 22:46:25 +0200

    tcp first implementation

--- Server/Server/TCPServer.cpp
+++ Server/Server/TCPServer.cpp
@@ -1,9 +1,16 @@
 #include "TCPServer.h"
 namespace OpenClouds
 {
-	TCPServer& TCPServer::Inst(){
+	TCPServer& TCPServer::Inst()
+	{
 		static TCPServer instance; // Guaranteed to be destroyed.
 		// Instantiated on first use.
 		return instance;
+	}
+
+	void TCPServer::Start()
+	{
+		sendBuffer = new Buffer(1024, 32);
+		recvBuffer = new Buffer(1024, 32);
 	}
 }
--- Server/Server/TCPServer.h
+++ Server/Server/TCPServer.h
@@ -1,5 +1,23 @@
 #pragma once
 
+
+#include "Buffer.h"
+#ifdef _WIN32
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+#define INVALID_SOCKET (~0)
+#define SOCKET_ERROR (-1)
+#endif
+
+
 /*
 * TCP Server handles tcp packets. YOLO
 * reads packets from users who will:
@@ -10,15 +28,46 @@
 
 namespace OpenClouds
 {
+
+	typedef unsigned int Socket;
+
+
+	class Client
+	{
+	public:
+		Socket socket;
+		struct sockaddr_in sockaddr;
+		unsigned short port;
+		Buffer* buffer;
+
+
+		Client();
+		~Client();
+		
+
+	};
+
 	class TCPServer
 	{
 	public:
 		static TCPServer& Inst();
+		
+		void Start();
+
+		Buffer* sendBuffer;
+		Buffer* recvBuffer;
 
 	private:
-		TCPServer(){};// Private so that it can  not be called
+		TCPServer(){};// Private: can not be called
 		void operator=(TCPServer const&); // override assignment operator 
 	};
+
+	// static instance of the TCPServer
 	static TCPServer& Server = TCPServer::Inst();
+
+
+
+
+
 }
 
