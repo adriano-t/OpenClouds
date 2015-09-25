47de347c36f4a476cf9b05e815204c19ede18c8d
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-09-25 21:30:52 +0200

    created client project

---
+++ Client/Client/Client/Client.h
@@ -0,0 +1,10 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include "Includes.h"
+class Client
+{
+public:
+	Client();
+private:
+};
+#endif // CLIENT_H
---
+++ Client/Client/Client/Main.cpp
@@ -0,0 +1,14 @@
+#include "Client.h"
+
+/*
+What does client do?
+1) At startup perform a full sync with the remote server
+2) React to file edition/creation/deletion updating the remote server
+
+*/
+
+
+int main()
+{
+
+}
